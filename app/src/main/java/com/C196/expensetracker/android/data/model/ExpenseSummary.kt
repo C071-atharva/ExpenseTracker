49c9f1f4f4af9f5fafb8cbdc62efea3a772201dd
Atharva <atharva.chavan1602@gmail.com>
2025-11-10 17:53:53 +0530

    Project Setup.

---
+++ app/src/main/java/com/C196/expensetracker/android/data/model/ExpenseSummary.kt
@@ -0,0 +1,7 @@
+package com.C196.expensetracker.android.data.model
+
+data class ExpenseSummary(
+    val type: String,
+    val date: String,
+    val total_amount: Double
+)
---
+++ app/src/main/java/com/C196/expensetracker/android/feature/stats/StatsViewModel.kt
@@ -0,0 +1,28 @@
+package com.C196.expensetracker.android.feature.stats
+
+import com.C196.expensetracker.android.base.BaseViewModel
+import com.C196.expensetracker.android.base.UiEvent
+import com.C196.expensetracker.android.utils.Utils
+import com.C196.expensetracker.android.data.dao.ExpenseDao
+import com.C196.expensetracker.android.data.model.ExpenseSummary
+import com.github.mikephil.charting.data.Entry
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class StatsViewModel @Inject constructor(val dao: ExpenseDao) : BaseViewModel() {
+    val entries = dao.getAllExpenseByDate()
+    val topEntries = dao.getTopExpenses()
+    fun getEntriesForChart(entries: List<ExpenseSummary>): List<Entry> {
+        val list = mutableListOf<Entry>()
+        for (entry in entries) {
+            val formattedDate = Utils.getMillisFromDate(entry.date)
+            list.add(Entry(formattedDate.toFloat(), entry.total_amount.toFloat()))
+        }
+        return list
+    }
+
+    override fun onEvent(event: UiEvent) {
+    }
+}
+
